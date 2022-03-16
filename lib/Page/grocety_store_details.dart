6fb218462a7a513f554e902327ead5926e5c4326
Rkira7 <ricardo_71190@hotmail.com>
2022-03-15 21:12:55 -0600

    cambiar details a stateless

--- lib/Page/grocety_store_details.dart
+++ lib/Page/grocety_store_details.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:grocerystore/grocery_product.dart';
+import 'package:grocerystore/grocery_provider.dart';
 
 import '../Bloc/grocery_store_bloc.dart';
 
-class GroceryStoreDetails extends StatefulWidget {
+class GroceryStoreDetails extends StatelessWidget {
   final GroceryProduct product;
   final VoidCallback onProductAdded;
 
@@ -11,22 +12,8 @@ class GroceryStoreDetails extends StatefulWidget {
       : super(key: key);
 
   @override
-  State<GroceryStoreDetails> createState() => _GroceryStoreDetailsState();
-}
-
-class _GroceryStoreDetailsState extends State<GroceryStoreDetails> {
-
-  @override
-  void dispose() {
-    bloc.streamTag;
-    // TODO: implement dispose
-    super.dispose();
-  }
-
-  final bloc = GroceryStoreBloc();
-
-  @override
   Widget build(BuildContext context) {
+    final bloc = GroceryStoreBloc();
     return Scaffold(
       appBar: AppBar(
         leading: const BackButton(
@@ -36,94 +23,98 @@ class _GroceryStoreDetailsState extends State<GroceryStoreDetails> {
         elevation: 0,
       ),
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  StreamBuilder<String>(
-                    stream: bloc.streamTag,
-                    initialData: "",
-                    builder: (context, heroTag) {
-                      return Center(
-                          child: Hero(
-                        tag: "list_${widget.product.name}_${heroTag.data}",
-                            child: Image.network(
-                              widget.product.image!,
-                              height: MediaQuery.of(context).size.height * 0.3,
-                              fit: BoxFit.contain,
-                            ),
-                      ));
-                    }
-                  ),
+      body: GroceryProvider(
+        bloc: bloc,
+        child: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    StreamBuilder<String>(
+                        stream: bloc.streamTag,
+                        initialData: "",
+                        builder: (context, heroTag) {
+                        return Center(
+                            child: Hero(
+                              tag: "list_${product.name}_${heroTag.data}",
+                              child: Image.network(
+                                product.image!,
+                                height: MediaQuery.of(context).size.height * 0.3,
+                                fit: BoxFit.contain,
+                              ),
+                            ));
+                      }
+                    ),
 
-                  Text("${widget.product.name}",
-                    style:
-                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-                  ),
-                  const SizedBox(height: 15),
-                  Text("\$${widget.product.weight}",
-                    style: const TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 20,
-                        color: Colors.grey),
-                  ),
-                  Row(
-                    children: [
-                      const Spacer(),
-                      Text("\$${widget.product.price}",
-                        style:
-                        const TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 15),
-                  const Text(
-                    "About the product",
-                    style:
-                    TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  ),
-                  const SizedBox(height: 15),
-                  Text(
-                    "${widget.product.decription}",
-                    style:
-                        const TextStyle(fontSize: 20),
-                  )
-                ],
+
+                    Text("${product.name}",
+                      style:
+                      const TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                    ),
+                    const SizedBox(height: 15),
+                    Text("\$${product.weight}",
+                      style: const TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 20,
+                          color: Colors.grey),
+                    ),
+                    Row(
+                      children: [
+                        const Spacer(),
+                        Text("\$${product.price}",
+                          style:
+                          const TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 15),
+                    const Text(
+                      "About the product",
+                      style:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                    const SizedBox(height: 15),
+                    Text(
+                      "${product.decription}",
+                      style:
+                      const TextStyle(fontSize: 20),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Row(
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: IconButton(
-                      onPressed: () => " ",
-                      icon: const Icon(Icons.favorite_border)),
-                ),
-                Expanded(
-                  flex: 4,
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                        shape:  MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(borderRadius: BorderRadius.circular(30))),
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(const Color(0xfff4c459)),),
-                      onPressed: () {
-                        bloc.addToCart(context, widget.onProductAdded);
-                      },
-                      child: const Text("Add to cart",
-                        style: TextStyle(
-                          color: Colors.black),),
-                ))
-              ],
-            ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: IconButton(
+                        onPressed: () => " ",
+                        icon: const Icon(Icons.favorite_border)),
+                  ),
+                  Expanded(
+                      flex: 4,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          shape:  MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(borderRadius: BorderRadius.circular(30))),
+                          backgroundColor:
+                          MaterialStateProperty.all<Color>(const Color(0xfff4c459)),),
+                        onPressed: () {
+                          bloc.addToCart(context, onProductAdded);
+                        },
+                        child: const Text("Add to cart",
+                          style: TextStyle(
+                              color: Colors.black),),
+                      ))
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
